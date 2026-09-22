95e02c600ae4cc0128b49d29247889e440773b44
Daniel - Laptop <daniel.balasingam1@gmail.com>
2026-09-22 20:14:02 +1200

    Build login page UI and styling

--- client/app/routes/auth/login.tsx
+++ client/app/routes/auth/login.tsx
@@ -1,7 +1,63 @@
+import React, { useState } from 'react';
+
+interface FormData {
+  username: string;
+  password: string;
+}
+
 export default function Login() {
+  const [formData, setFormData] = useState<FormData>({
+    username: '',
+    password: '',
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = event.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: type === 'checkbox' ? checked : value,
+    }));
+  };
+
+  const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Submitted Data:', formData);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="container">
+      <h1 className="login-header">NZ Animal Sightings Dashboard</h1>
+      <div className="login-box">
+        <form onSubmit={handleLogin}>
+          <h2>Login</h2>
+          <div className="form-group">
+            <label htmlFor="username">Username: </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password: </label>
+            <input
+              name="password"
+              type="password"
+              id="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary btn-block">
+              <span>Login</span>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- client/app/routes/home.tsx
+++ client/app/routes/home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Sighting Dashboard</h1>
+    <div className="container">
+      <h1>NZ Animal Sightings Dashboard</h1>
     </div>
   );
 }
